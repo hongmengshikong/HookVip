e66e3942460f43087a35c5f81291a831797735f7
hongmengshikong <2152085933@qq.com>
2026-05-05 20:17:54 +0800

    更新版本号

--- HookVIP/app/build.gradle.kts
+++ HookVIP/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.hook.vip"
         minSdk = 28
         targetSdk = 35
-        versionCode = 17
-        versionName = "1.7"
+        versionCode = 18
+        versionName = "1.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
